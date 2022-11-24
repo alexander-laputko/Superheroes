be394dd53692715d281d5fe295f3ca84a9deac9b
alexander.laputko <alexander.laputko@ya.ru>
2022-11-24 18:21:49 +0300

    add list heroes

--- app/src/main/java/com/alexa/superheroes/MainActivity.kt
+++ app/src/main/java/com/alexa/superheroes/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -17,9 +19,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alexa.superheroes.model.Hero
+import com.alexa.superheroes.model.HeroesRepository
 import com.alexa.superheroes.ui.theme.Shapes
 import com.alexa.superheroes.ui.theme.SuperheroesTheme
 
@@ -33,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    HeroList(heroes = HeroesRepository.heroes)
                 }
             }
         }
@@ -43,22 +47,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
     Card(
-        modifier = Modifier.padding(16.dp),
         elevation = 2.dp,
+        modifier = modifier.padding(8.dp)
     ) {
         Row(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(16.dp)
+                .sizeIn(minHeight = 72.dp),
         ) {
-            Column {
-                Text(text = stringResource(id = hero.name), style = MaterialTheme.typography.h3)
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(id = hero.name),
+                    style = MaterialTheme.typography.h3
+                )
                 Text(
                     text = stringResource(id = hero.description),
-                    style = MaterialTheme.typography.body1
+                    style = MaterialTheme.typography.body1,
                 )
             }
-            Spacer(modifier = Modifier.padding(16.dp))
+            Spacer(Modifier.width(16.dp))
             Box(
                 modifier = Modifier
                     .size(72.dp)
@@ -69,9 +77,18 @@ fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
                     painter = painterResource(hero.image),
                     contentDescription = null,
                     alignment = Alignment.TopCenter,
-                    contentScale = ContentScale.FillWidth
+                    contentScale = ContentScale.FillWidth,
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun HeroList(heroes: List<Hero>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(heroes) {
+            HeroItem(hero = it)
         }
     }
 }
@@ -87,6 +104,14 @@ fun HeroItemPreview() {
                 image = R.drawable.android_superhero2
             )
         )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HeroListPreview() {
+    SuperheroesTheme() {
+        HeroList(heroes = HeroesRepository.heroes)
     }
 }
 
--- app/src/main/java/com/alexa/superheroes/model/HeroesDataSource.kt
+++ app/src/main/java/com/alexa/superheroes/model/HeroesDataSource.kt
@@ -33,6 +33,21 @@ object HeroesRepository {
             name = R.string.hero6,
             description = R.string.description6,
             image = R.drawable.android_superhero6
+        ),
+        Hero(
+            name = R.string.hero2,
+            description = R.string.description2,
+            image = R.drawable.android_superhero2
+        ),
+        Hero(
+            name = R.string.hero3,
+            description = R.string.description3,
+            image = R.drawable.android_superhero3
+        ),
+        Hero(
+            name = R.string.hero4,
+            description = R.string.description4,
+            image = R.drawable.android_superhero4
         )
     )
 }
