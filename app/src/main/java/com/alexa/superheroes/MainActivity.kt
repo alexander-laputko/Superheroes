d0ad691bed1df1b0d9768b8d571adc0f2877d70f
alexander.laputko <alexander.laputko@ya.ru>
2022-11-24 19:03:46 +0300

    add themes status bar

--- app/src/main/java/com/alexa/superheroes/MainActivity.kt
+++ app/src/main/java/com/alexa/superheroes/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    HeroList(heroes = HeroesRepository.heroes)
+                    SuperheroApp()
                 }
             }
         }
